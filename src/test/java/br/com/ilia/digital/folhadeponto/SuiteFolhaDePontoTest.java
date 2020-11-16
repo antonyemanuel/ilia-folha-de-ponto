2efd1d112bda0c4c652daa817a810d606d083aad
Antônio Emanuel <Emanuel>
2020-11-16 12:50:29 -0300

    criacao de testes e configuracoes: add test no pom.xml, criação do application-test.properties; FolhaDePOntoControllerIntegrationTest: alguns testes do controller; SuiteFolhaDePontoTest: para ajudar na execucao ffutura dos testes

---
+++ src/test/java/br/com/ilia/digital/folhadeponto/SuiteFolhaDePontoTest.java
@@ -0,0 +1,14 @@
+package br.com.ilia.digital.folhadeponto;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import br.com.ilia.digital.folhadeponto.controller.FolhaDePontoControllerIntegrationTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	FolhaDePontoControllerIntegrationTest.class
+})
+public class SuiteFolhaDePontoTest {
+
+}
